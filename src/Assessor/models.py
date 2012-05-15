3f0e317ae2b8788b1d3fa22c38e298073c5fbb7c
Justin Chudgar <justin@justinzane.com>
2012-05-14 18:02:31 -0700

    Initial commit. Nothing useful yet.

---
+++ src/Assessor/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+#from django.contrib.auth.models import User
+
+class Question(models.Model):
+    text = models.TextField()
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    text = models.TextField()
+    is_correct = models.BooleanField()
+    
+class Quiz(models.Model):
+#    user = models.ForeignKey(User)
+    num_questions = models.IntegerField()
+    num_correct = models.IntegerField()
+    
+class QuizQuestions(models.Model):
+    quiz = models.ForeignKey(Quiz)
+    question = models.ForeignKey(Question)
+
---
+++ src/assessor/settings.py
@@ -0,0 +1,91 @@
+import os
+BASE_DIR = os.path.abspath(os.path.curdir)
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+ADMINS = (('Justin Chudgar', 'justin@justinzane.com'),)
+MANAGERS = ADMINS
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '', # Or path to database file if using sqlite3.
+        'USER': '', # Not used with sqlite3.
+        'PASSWORD': '', # Not used with sqlite3.
+        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+    }
+}
+TIME_ZONE = None
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+MEDIA_ROOT = ''
+MEDIA_URL = ''
+
+STATIC_ROOT = BASE_DIR + '/deploy-static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ()
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+SECRET_KEY = 'xosuyg!f^ef4w_)3nz2uvm$^)#8*owd!1g$c4q636mizhoj1ah'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'assessor.urls'
+WSGI_APPLICATION = 'assessor.wsgi.application'
+TEMPLATE_DIRS = ()
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'tastypie',
+    'Assessor'
+)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
